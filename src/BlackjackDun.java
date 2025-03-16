ed3e7b8beff43fd9c0661884f1d9df92382ec75e
Orgienleiter <157519458+Orgienleiter@users.noreply.github.com>
2025-03-16 22:39:45 +0100

    Fight gefixt

--- src/BlackjackDun.java
+++ src/BlackjackDun.java
@@ -1,9 +1,9 @@
 public class BlackjackDun {
     public static void main(String[] args) throws Exception {
-        Fight f = new Fight(10, 4);
+        Fight f = new Fight(1, 2, 4);
 
         for (int i = 0; i < 100; i++) {
-            f.draw();
+            f.drawRight();
         }
 
     }
--- src/Fight.java
+++ src/Fight.java
@@ -1,26 +1,39 @@
 public class Fight {
     int[] deck;
+    int minValue;
     int maxValue;
     int cardCount;
 
-    public Fight(int maxValue, int cardCount) {
-        this.cardCount = cardCount + 1;
+    public Fight(int minValue, int maxValue, int cardCount, boolean images) {
+        this.cardCount = cardCount;
+        this.minValue = minValue;
         this.maxValue = maxValue;
-        this.deck = new int[maxValue];
+
+        if (images) {
+            this.deck = new int[1 + maxValue - minValue + 3];
+        }
+        this.deck = new int[1 + maxValue - minValue]; // D B und K fehlen
+
         for (int i = 0; i < deck.length; i++) {
             this.deck[i] = this.cardCount;
         }
     }
 
-    public void draw() {
+    public int draw() {
         int card;
-        // System.out.println("Anzahl: " + this.deck[1]);
+        card = (int) (Math.random() * this.maxValue) + this.minValue;
+        return card;
+    }
+
+    public void drawRight() {
+        int card;
+
         do {
-            card = (int) (Math.random() * this.maxValue) + 1;
+            card = draw();
             this.deck[card - 1] = this.deck[card - 1] - 1;
-            // System.out.println("Count: " + this.deck[card - 1]);
-        } while (this.deck[card - 1] <= 0);
+        } while (this.deck[card - 1] < 0);
 
         System.out.print(card + "\t");
+        // System.out.print("Anzahl: " + this.deck[card - 1] + "\n");
     }
 }
